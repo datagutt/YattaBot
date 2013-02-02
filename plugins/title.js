b38e2cf8c5885237ea96f7c5ee78cb34550b9fbf
datagutt <datagutt666@hotmail.com>
2013-02-02 22:12:50 +0100

    fix title fix

--- plugins/title.js
+++ plugins/title.js
@@ -1,8 +1,14 @@
 var request = require('request');
 module.exports = function(bot){
 	bot.on('privmsg', function(event){
-		if(match = event.message.match(/\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?]))/i) && event.source.nick !== 'clibot' && event.source.nick !== bot.nick){
+		if(match = event.message.match(/\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?]))/i)){
 			var url = match[0], title;
+			if(!url){
+				return;
+			}
+			if(event.source.nick == 'clibot' || event.source.nick == bot.nick){
+				return;
+			}
 			if(url.indexOf('http') !== 0){
 				url = 'https://' + url;
 			}
