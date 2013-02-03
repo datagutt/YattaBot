127c5c64e77f9285e9af12964128408e68a41067
datagutt <datagutt666@hotmail.com>
2013-02-03 19:09:32 +0100

    Show tipage host in title plugin

--- plugins/title.js
+++ plugins/title.js
@@ -1,4 +1,8 @@
 var request = require('request');
+var getHostname = function(str){
+	var re = new RegExp('^(?:f|ht)tp(?:s)?\://([^/]+)', 'im');
+	return str.match(re)[1].toString();
+}
 module.exports = function(bot){
 	bot.on('privmsg', function(event){
 		if(match = event.message.match(/\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?]))/i)){
@@ -17,7 +21,7 @@ module.exports = function(bot){
 					var match = /<title>(.*)<\/title>/.exec(body);
 					if(match && match[1]){
 						title = match[1];
-						bot.message(event.target, title);
+						bot.message(event.target, getHostname(url) + ': ' + title);
 					}
 				}
 			});
