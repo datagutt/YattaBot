17ae679eebd91b3f95b8abad9b7b7c137f8a6362
datagutt <datagutt666@hotmail.com>
2013-02-05 13:42:08 +0100

    Dood, don't part, kick

--- bot.js
+++ bot.js
@@ -72,7 +72,7 @@ Bot.prototype = {
 	},
 	kick: function(channel, user, message){
 		var self = this;
-		IRC.part(self.session, channel, user, message);
+		IRC.kick(self.session, channel, user, message);
 	},
 	ban: function(channel, user){
 		var self = this;
