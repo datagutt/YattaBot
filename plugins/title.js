375c7b343bf137971d7a06bb38dd34a69e5d178a
datagutt <datagutt666@hotmail.com>
2013-02-08 17:12:48 +0100

    Fix the title bugs

--- plugins/title.js
+++ plugins/title.js
@@ -5,7 +5,7 @@ var getHostname = function(str){
 };
 var checkForLinks = function(event, bot){
 	if(match = event.message.match(/\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?]))/i)){
-			var url = match[0], title;
+			var url = match[0], title, hostname;
 			if(!url){
 				return;
 			}
@@ -15,13 +15,19 @@ var checkForLinks = function(event, bot){
 			if(url.indexOf('http') !== 0){
 				url = 'https://' + url;
 			}
+			console.log(url);
+			hostname = getHostname(url);
+			if(!hostname){
+				return;
+			}
 			var options = {
-				host: getHostname(url),
+				host: hostname,
 				port: 80,
-				path: url.replace(getHostname(url), ''),
+				path: '/' + url.split(/\/+/g).slice(2).join('/'),
 				method: 'GET'
 			};
-			var req = http.get(options, function(res) {
+			console.log(options);
+			var req = http.get(options, function(res){
 				var body = '', contentType = res.headers['content-type'].split(';');
 				res.setEncoding('utf8');
 				if(res.statusCode !== 200 && res.statusCode !== 304 && res.statusCode !== 400){
@@ -43,7 +49,7 @@ var checkForLinks = function(event, bot){
 				});
 				res.on('end', function(){
 					if(body){
-						var match = /<head.*?>.*?<title.*?>(.*?)<\/title>.*?<\/head>/.exec(body.replace(/(\r\n|\n|\r)/gm,'')), redirected;
+						var match = /<head.*?>.*?<title.*?>(.*?)<\/title>.*?<\/head>/im.exec(body.replace(/(\r\n|\n|\r)/gm,'')), redirected;
 						if(match && match[1]){
 							title = match[1];
 							if(title.length <= 0){
@@ -56,7 +62,7 @@ var checkForLinks = function(event, bot){
 						}
 					}
 				});
-			});
+			}).on('error', function(e){});
 		}
 };
 module.exports = function(bot){
@@ -68,7 +74,9 @@ module.exports = function(bot){
 		if(validChannels){
 			validChannels.forEach(function(channel){
 				if(event.target == channel){
-					checkForLinks(event, bot);
+					try{
+						checkForLinks(event, bot);
+					}catch(e){}
 				}
 			});
 		}
