e45a62ae0d37dd81a6217da2d1ad7d43ba5cb595
datagutt <datagutt@lekanger.no>
2013-05-18 16:59:53 +0200

    Increase max users you can dood

--- plugins/troll.js
+++ plugins/troll.js
@@ -3,7 +3,7 @@ module.exports = function(bot){
 	bot.addCommand('dood', 'dood', '[<user>]', USER_LEVEL_NORMAL, false, function(event){
 		if(!event.params[0]){
 			bot.message(event.target, toSay);
-		}else if(event.params && event.params.length < 3){
+		}else if(event.params && event.params.length < 5){
 			event.params.forEach(function(user){
 				bot.message(event.target, user + ',');
 			});
