6e3513fd0ed549ef61053af26c7bdd9194066336
datagutt <datagutt@lekanger.no>
2013-06-11 17:50:25 +0200

    implement sandbox for eval

--- plugins/eval.js
+++ plugins/eval.js
@@ -1,21 +1,13 @@
-var util = require('util');
-var evaluate = function(code){
-	try{
-		return eval(code);
-	}catch(e){
-		return e;
-	}
-};
+var Sandbox = require('sandbox'),
+	s = new Sandbox()
 module.exports = function(bot){
 	bot.addCommand('eval', 'Executes the current code', '<code>', USER_LEVEL_OWNER, false, function(event){
 		var code;
 		if(event.params){
 			code = event.params.join(' ');
 		}
-		evaluated = evaluate.apply(this, [code]);
-		result = util.inspect(evaluated);
-		if(result){
-			bot.message(event.source.nick, result);
-		}
+		s.run(code, function(output){
+			bot.message(event.target, output.result);
+		})
 	});
 };
